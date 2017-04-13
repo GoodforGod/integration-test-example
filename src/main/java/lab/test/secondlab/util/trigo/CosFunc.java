f00ad8db87623443d375abf8add560a50538562c
GoodforGod <goodforgod.dev@gmail.com>
2017-04-13 23:56:39 +0300

    - Cos

--- src/main/java/lab/test/secondlab/util/trigo/CosFunc.java
+++ src/main/java/lab/test/secondlab/util/trigo/CosFunc.java
@@ -4,12 +4,20 @@ package lab.test.secondlab.util.trigo;
  * Created by @GoodforGod on 13.04.2017.
  */
 
+import lab.test.secondlab.util.Computer;
+
 /**
  * Default Comment
  */
 public class CosFunc implements IFunc {
 
     public double calc(double x) {
-        return 0;
+        double result = 0;
+        int mark = -1;
+
+        for(int i = 0; i <= STEP * 4; i += 2)
+            result += Math.pow(x, i) / Computer.factorial(i) * (mark = mark == 1 ? -1 : 1);
+
+        return result;
     }
 }
