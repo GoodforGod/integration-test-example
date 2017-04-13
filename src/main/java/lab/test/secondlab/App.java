84bb8c5cce7d1f0efa3ecfac45b6b1d0807d65fa
GoodforGod <goodforgod.dev@gmail.com>
2017-04-14 00:01:32 +0300

    - 1 & 2 Formula func

--- src/main/java/lab/test/secondlab/App.java
+++ src/main/java/lab/test/secondlab/App.java
@@ -4,6 +4,15 @@ package lab.test.secondlab;
  * Compute!
  */
 public class App {
+
+    public static double firstFormula() {
+        return 0;
+    }
+
+    public static double secondFormula() {
+        return 0;
+    }
+
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
