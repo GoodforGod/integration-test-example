b577e776b8424863d94a0e27a42b805be5a887c4
Kirill <whobscr@gmail.com>
2017-04-28 19:53:14 +0300

    added last tests into the incremental suite

--- src/test/java/lab/test/secondlab/util/formula/IncrementalSuite.java
+++ src/test/java/lab/test/secondlab/util/formula/IncrementalSuite.java
@@ -1,5 +1,7 @@
 package lab.test.secondlab.util.formula;
 
+import lab.test.secondlab.util.formula.first.TestFirstFormula;
+import lab.test.secondlab.util.formula.second.TestSecondFormula;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -10,6 +12,9 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         FirstLevelTest.class,
-        SecondLevelTest.class})
+        SecondLevelTest.class,
+        TestFirstFormula.class,
+        TestSecondFormula.class,
+})
 public class IncrementalSuite {
 }
